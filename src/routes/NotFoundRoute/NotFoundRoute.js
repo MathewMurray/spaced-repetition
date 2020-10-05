1a0eee5167f887c4e4a0b6fec5a2cd686eb6fb36
V <62076871+Nvlt@users.noreply.github.com>
2020-10-05 12:30:36 -0700

    css

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+require('./NotFoundRoute.css');
 class NotFoundRoute extends Component {
   render() {
     return (
-      <section>
+      <section className="notFound">
         <h2>404 - Page not found</h2>
-        <p>Try going back to your previous page.</p>
+        <p>Woah there, you just discovered a quantum tunnel into nothingness. Good job, I guess..</p>
       </section>
     );
   }
