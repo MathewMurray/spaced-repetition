af0063644c6c8b493425a09133f5ac236ea170d8
V <62076871+Nvlt@users.noreply.github.com>
2020-10-07 09:36:43 -0700

    day 3

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -3,51 +3,56 @@ import LS from '../../services/languageService';
 import LC from '../../contexts/LanguageContext';
 require('./DashboardRoute.css');
 class DashboardRoute extends Component {
-  state = {
-
-  }
-  componentDidMount = async () => {
+  static contextType = LC;
+  
+  componentDidMount = async()=>
+  {
     const langData = await LS.getLanguageData();
     this.context.setData(langData)
   }
-  populatePracticeWords = () => {
-    if (this.state.words) {
-      const words = this.state.words;
+  populatePracticeWords = ()=>
+  {
+    if(this.context.words)
+    {
+      const words = this.context.words;
       const html = [];
-      for (const word of words) {
+      for(const word of words)
+      {
         console.log(word);
-        html.push(<li key={ word.id }>
-          <h4>{ word.original }</h4>
-          <p>correct answer count: { word.correct_count }</p>
-          <p>incorrect answer count: { word.incorrect_count }</p>
-        </li>)
+        html.push(<li key={word.id}>
+                    <h4>{word.original}</h4>
+                    <p>correct answer count: {word.correct_count}</p>
+                    <p>incorrect answer count: {word.incorrect_count}</p>
+                  </li>)
       }
       return html;
     }
   }
-  render = () => {
-
-    if (this.state.language) {
-      const { name, user_id, id, head, total_score } = this.state.language;
+  render = ()=>{
+    
+    if(this.context.language)
+    {
+      const {name,total_score} = this.context.language;
       console.log(this.state);
       return (
         <section className="Dashboard">
-          <h2>{ name }</h2>
-          <h2>Total correct answers: { total_score }</h2>
+          <h2>{name}</h2>
+          <h2>Total correct answers: {total_score}</h2>
           <a href="/learn">Start practicing</a>
           <h3>Words to practice</h3>
           <ul>
-            { this.populatePracticeWords() }
+            {this.populatePracticeWords()}
           </ul>
         </section>
       );
     }
-    else {
-      return (<section className="Dashboard">
+    else
+    {
+      return(<section className="Dashboard">
         <h2>Error</h2>
       </section>)
     }
-
+    
   }
 }
 
