07bba53e8fd8c5c7ce566ba3548c323ecb4dbe6b
Mathew Murray <the.thran@gmail.com>
2020-10-09 15:14:45 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://safe-plains-90435.herokuapp.com',
+  API_ENDPOINT: 'https://safe-plains-90435.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
